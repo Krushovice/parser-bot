78adb8e083dd8e5b5687f11fbba57c5aafbea7f6
Krushovice <kickstar69@yandex.ru>
2025-06-29 13:24:34 +1000

    create lifespan func

---
+++ src/lifespan.py
@@ -0,0 +1,23 @@
+import logging
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, Request
+
+from bot import bot, dp
+from core.config import settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Код, выполняющийся при запуске приложения
+    webhook_url = settings.get_webhook_url()  # Получаем URL вебхука
+    await bot.set_webhook(
+        url=webhook_url,
+        allowed_updates=dp.resolve_used_update_types(),
+        drop_pending_updates=True,
+    )
+    logging.info(f"Webhook set to {webhook_url}")
+    yield  # Приложение работает
+    # Код, выполняющийся при завершении работы приложения
+    await bot.delete_webhook()
+    logging.info("Webhook removed")
