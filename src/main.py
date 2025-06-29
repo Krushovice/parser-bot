bf1a0705eef1788420db95ff495835a13c3c1bf7
Krushovice <kickstar69@yandex.ru>
2025-06-29 13:32:36 +1000

    create main app

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
-def print_hi(name):
+from fastapi import FastAPI, Request
 
-    print(f'Hi, {name}')
+from bot import bot, dp
+from lifespan import lifespan
+from utils.logger import setup_logger
+
+logger = setup_logger(__name__)
+app = FastAPI(lifespan=lifespan)
 
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
+# Маршрут для обработки вебхуков
+@app.post("/webhook")
+async def webhook(request: Request) -> None:
+    logger.info("Received webhook request")
+    update = await request.json()  # Получаем данные из запроса
+    # Обрабатываем обновление через диспетчер (dp) и передаем в бот
+    await dp.feed_update(bot, update)
+    logger.info("Update processed")
