f90535b505fd97ce15e9df328610d039f2b39aa0
Krushovice <kickstar69@yandex.ru>
2025-06-29 14:13:03 +1000

    add the main router to the lifespan function

--- src/lifespan.py
+++ src/lifespan.py
@@ -2,24 +2,28 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from bot import bot, dp
+from bot import bot, dp, start_bot, stop_bot
 from core.config import settings
 from utils.logger import setup_logger
+from routers import router as main_router
 
 logger = setup_logger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Код, выполняющийся при запуске приложения
-    webhook_url = settings.web.get_webhook_url  # Получаем URL вебхука
+    logger.info("Starting bot setup...")
+    dp.include_router(main_router)
+    await start_bot()
+    webhook_url = settings.web.get_webhook_url
     await bot.set_webhook(
         url=webhook_url,
         allowed_updates=dp.resolve_used_update_types(),
         drop_pending_updates=True,
     )
     logger.info(f"Webhook set to {webhook_url}")
-    yield  # Приложение работает
-    # Код, выполняющийся при завершении работы приложения
+    yield
+    logger.info("Shutting down bot...")
     await bot.delete_webhook()
-    logger.info("Webhook removed")
+    await stop_bot()
+    logger.info("Webhook deleted")
