f100e79b3ccdb0770a367f3416b2abb9f9f08761
Krushovice <kickstar69@yandex.ru>
2025-07-16 13:04:34 +1000

    add inline kb

--- src/markups/reply_keybords.py
+++ src/markups/reply_keybords.py
@@ -1,7 +1,20 @@
-from aiogram.types import ReplyKeyboardMarkup, WebAppInfo
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from enum import IntEnum, auto
+
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 from src.core.config import settings
+
+
+class CategoryActions(IntEnum):
+    details = auto()
+    back = auto()
+
+
+class CategoryCbData(CallbackData, prefix="cat"):
+    action: CategoryActions
+    name: str
 
 
 def main_keyboard(user_id: int) -> ReplyKeyboardMarkup:
@@ -10,7 +23,8 @@ def main_keyboard(user_id: int) -> ReplyKeyboardMarkup:
     # url_add_application = (
     #     f"{settings.BASE_SITE}/form?user_id={user_id}&first_name={first_name}"
     # )
-    # kb.button(text="🌐 Предложения по работе", web_app=WebAppInfo(url=url_applications))
+    kb.button(text="🌐 Предложения по работе")
+
     kb.button(text="ℹ️ О нас")
     if user_id == settings.bot.admin_id:
         kb.button(text="🔑 Админ панель")
@@ -21,5 +35,18 @@ def main_keyboard(user_id: int) -> ReplyKeyboardMarkup:
 def back_keyboard() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
     kb.button(text="🔙 Назад")
+    kb.adjust(1)
+    return kb.as_markup(resize_keyboard=True)
+
+
+def job_category_keyboard() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    kb.button(
+        text="🛠 Tехремонт",
+        callback_data=CategoryCbData(
+            action=CategoryActions.details,
+            name="techrepair",
+        ),
+    )
     kb.adjust(1)
     return kb.as_markup(resize_keyboard=True)
